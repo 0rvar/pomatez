511e1c9519b4b48eac3da8cf9eec44b2fb9d62b1
Sekwah <contact@sekwah.com>
2023-10-25 05:46:23 +0100

    wip: updater

--- app/tauri/src/main.rs
+++ app/tauri/src/main.rs
@@ -5,26 +5,32 @@ windows_subsystem = "windows"
 
 use tauri::RunEvent;
 use tauri_plugin_autostart::MacosLauncher;
-use tauri_plugin_global_shortcut::GlobalShortcutExt;
 use tauri_plugin_window;
 
 #[macro_use]
 mod commands;
 mod system_tray;
 mod global_shortcuts;
+mod updater;
 
 use commands::PomatezCommands;
 use system_tray::PomatezTray;
 use global_shortcuts::{PomatezGlobalShortcutsSetup, PomatezGlobalShortcutsRegister};
+use crate::updater::PomatezUpdater;
 
 fn main() {
     let app = tauri::Builder::default()
         .plugin(tauri_plugin_autostart::init(MacosLauncher::LaunchAgent, None))
         .plugin(tauri_plugin_window::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .register_pomatez_commands()
         .setup(|app| {
-            app.setup_global_shortcuts();
-            app.set_pomatez_system_tray();
+            #[cfg(desktop)]
+            {
+                app.setup_global_shortcuts();
+                app.set_pomatez_system_tray();
+                app.check_for_update();
+            }
 
             Ok(())
         })
@@ -33,6 +39,7 @@ fn main() {
 
     app.run(|app_handle, e| match e {
         RunEvent::Ready => {
+            #[cfg(desktop)]
             app_handle.register_global_shortcuts();
             println!("Pomatez is ready");
         }
